67523aa03aa046cc55774b6e97cd434c982a579a
denkin14 <dennismuguongo@gmail.com>
2022-04-08 14:06:06 +0000

    exclude q and e lower case alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print lowercase alphabet excluding q and e
+ * Return: 0
+ */
+int main(void)
+{
+char d;
+
+for(d = 'a'; c <= 'z'; d++)
+{
+if (d != 'e' && d != 'q')
+putchar(d);
+}
+
+putchar('\n');
+
+return (0);
+}
+
