2562d86c6463e2517b969f660a05055a648d35c7
denkin14 <dennismuguongo@gmail.com>
2022-04-08 14:26:23 +0000

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print no 0 to 9 on new line
+ * Return: 0
+ */
+int main(void)
+{
+int d;
+
+for (d = 0; d <= 9; ++d)
+putchar('0' + d);
+
+putchar('\n');
+
+return (0);
+}
+	
