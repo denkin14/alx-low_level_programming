149a2317af1ae39883266e734f5d0d12b5ff3565
denkin14 <dennismuguongo@gmail.com>
2022-04-08 13:26:22 +0000

    print lower case alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main- entry point
+ * main - entry point
  *
- * Description - Print lowercase alphabets
+ * Description: Print lowercase alphabets
  * Return: 0
  * /
 int main(void)
