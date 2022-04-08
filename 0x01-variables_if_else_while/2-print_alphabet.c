1be332dade82eb619401067232fcb831b4e8c6df
denkin14 <dennismuguongo@gmail.com>
2022-04-08 13:17:44 +0000

    lower case alphabets only

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description - Print lowercase alphabets
+ */
+int main(void)
+{
+int d;
+
+for (d = 0; d < 26; ++d)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
+
