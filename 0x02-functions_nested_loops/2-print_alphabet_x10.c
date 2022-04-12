deeaf404e709cceb3c1294a52924a5964cb2f5f4
denkin14 <dennismuguongo@gmail.com>
2022-04-12 13:58:00 +0000

    2. print alphabet X10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 
- * Description: function uses _putchar fuction to print
+ * Description: print_alphabet_x10 function uses _putchar fuction to print
  */
 void print_alphabet_x10(void)
 {
@@ -11,8 +10,9 @@ int kinyua;
 
 for (kinyua = 0; kinyua < 10; kinyua++)
 {
-	for (dennis = 'a'; dennis <= 'z'; dennis++)
-		_putchar(dennis);
-	_putchar('\n');
+for (dennis = 'a'; dennis <= 'z'; dennis++)
+_putchar(dennis);
+
+_putchar('\n');
 }
 }
