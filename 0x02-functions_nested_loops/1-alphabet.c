8ba214e47c441ff89e744262c9b0149e54382ee1
denkin14 <dennismuguongo@gmail.com>
2022-04-12 13:27:54 +0000

    1.print alphabets in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print alphabets- all in lowercase followed by a new line
+ */
+void print_alphabet(void)
+{
+char dennis;
+
+for (dennis = 'a'; dennis <= 'z'; dennis++)
+_putchar(dennis);
+
+_putchar('\n');
+}
