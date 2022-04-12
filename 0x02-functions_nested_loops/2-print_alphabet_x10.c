8e67ce6823cfc59da8d0cfe8c8a549a1442942e9
denkin14 <dennismuguongo@gmail.com>
2022-04-12 13:54:47 +0000

    2.print alphabets ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,9 @@
 /**
  * print_alphabet_x10 
  * Description: function uses _putchar fuction to print
- * alphabet in lowercase 10 times
  */
-
 void print_alphabet_x10(void)
 {
-
 char dennis;
 int kinyua;
 
