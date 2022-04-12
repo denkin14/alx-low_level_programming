d3026a6416ce262ef9ff40d483615b2f6b56ef57
denkin14 <dennismuguongo@gmail.com>
2022-04-12 14:31:10 +0000

    4. ia an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if a character is alphabetic
+ * @c: The Character to be checked
+ * Return: 1 if character is letter, lower or upper case,0  otherwise
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'a' || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
