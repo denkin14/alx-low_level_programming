d63f1931f98d894f16b7252878442e72e8b5b647
denkin14 <dennismuguongo@gmail.com>
2022-04-12 13:41:25 +0000

    1. print alphabets in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print alphabets- folowed by a new line
- *
- * Description: function that prints alphabets in lower case
+ * print_alphabet- function that prints alphabets in lower case
+ *e
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
