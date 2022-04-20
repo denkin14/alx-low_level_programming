e6cfd22998304445b90a8e64df79fb11742cfbb8
denkin14 <dennismuguongo@gmail.com>
2022-04-20 11:48:50 +0000

    0. 98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates integer value to 98
+ * @n: A pointer to an to be updated
+ *
+ * Return : success
+ */
+
+void reset_to_98(int *n)
+{
+*n =98;
+}
