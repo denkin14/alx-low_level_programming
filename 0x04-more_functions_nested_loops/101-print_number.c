3edc99536f370086545d77dec0b85cabf3f04e27
denkin14 <dennismuguongo@gmail.com>
2022-04-20 09:54:03 +0000

    numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - prints ad integer
- * @n: input integer
+ * print_number - prints an integer.
+ * @n: input integer.
  * Return: no return.
  */
 void print_number(int n)
@@ -24,12 +24,12 @@ void print_number(int n)
 
 	while (d > 9)
 	{
-		d/= 10;
+		d /= 10;
 		count *= 10;
 	}
 
-	for (; count >= 1; count/=10)
+	for (; count >= 1; count /= 10)
 	{
-		_putchar(((m / count) % 10 + 48);
+		_putchar(((m / count) % 10) + 48);
 	}
 }
