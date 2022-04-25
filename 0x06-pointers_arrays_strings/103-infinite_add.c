f2e77193702d9a167e6b39fc80b1fd415e36e045
denkin14 <dennismuguongo@gmail.com>
2022-04-25 12:08:09 +0000

    103-infinite_add

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - functuion tomprint buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int d, m, k;
+
+	d = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (d < size)
+	{
+		m = size - d < 10 ? size - d : 10;
+		printf("%08x: ", d);
+		for (k = 0; k < 10; k++)
+		{
+			if (k < m)
+				printf("%02x", *(b + d +k));
+			else
+				printf(" ");
+			if (k % 2)
+			{
+				printf(" ");
+			}
+		}
+
+		for (k = 0; k < m; k++)
+		{
+			int c =*(b + d + k);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		d += 10;
+	}
+}
+
