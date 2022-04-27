dc6f138d6558744792296fe5c51eede3d9fd23fe
denkin14 <dennismuguongo@gmail.com>
2022-04-27 00:12:59 +0000

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies from source to dest values in array
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: n amount of times
+ * Return: return the string value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int d;
+
+	for (d = 0; d < n; d++)
+	{
+		dest[d] = src[d];
+	}
+	return (dest);
+}
