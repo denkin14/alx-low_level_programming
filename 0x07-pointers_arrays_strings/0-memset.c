97e1189b6e418ae48408473136d93a584b2f796d
denkin14 <dennismuguongo@gmail.com>
2022-04-27 00:07:47 +0000

    fuction fils memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _memset - sets memory to array
+ * _memset - function that sets memory to array
  * @s: array to set
  * @b: value to set it as
  * @n: n amount of times
- * Return: char value of str
+ * Return: char value of s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int t;
 
 	for (t = 0; t < n; t++)
-		str[t] = b;
-	return (str);
+		s[t] = b;
+	return (s);
 }
