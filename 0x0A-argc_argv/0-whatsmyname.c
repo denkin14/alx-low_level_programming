7096d860965688047636023ce8753efd3cfb6da2
denkin14 <dennismuguongo@gmail.com>
2022-05-04 01:06:03 +0000

    2. The best argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,11 @@
  * main - will Print number of arguments passed to it.
  * @argc: number of arguments sent to program.
  * @argv: array of pointers 
- * Return: Always 0 (Success)
+ * Return: Zero Success
  */
 int main(int argc, char *argv[])
 {
 (void) argc;
 printf("%s\n", argv[0]);
-
 return (0);
 }
