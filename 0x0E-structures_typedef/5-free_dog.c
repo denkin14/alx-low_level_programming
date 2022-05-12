cf320b125b8a7eb0b9d939d992d5c8dfb5179c3c
denkin14 <dennismuguongo@gmail.com>
2022-05-12 12:53:44 +0000

    last task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog: will free dogs
+ * @d:Struct Dog
+ *
+ * Return: no return
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
