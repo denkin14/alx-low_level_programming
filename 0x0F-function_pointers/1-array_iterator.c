30f2c2f65f9f9349cb460b70f55eab89db7d31af
denkin14 <dennismuguongo@gmail.com>
2022-05-16 10:56:08 +0000

    1. dont spend time too much on one thing

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - function to execute function given as parameter
+ * @array: our array of elements
+ * @size: the size of array
+ * @action: the function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int d;
+
+	if (array && size > 0 && action)
+	{
+		for (d = 0; d < size; d++)
+		{
+			action(array[d])
+		}
+	}
+}
