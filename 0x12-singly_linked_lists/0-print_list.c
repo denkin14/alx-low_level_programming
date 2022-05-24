ad8fbf12518e8858c02972b825020c66c5c38d6c
denkin14 <dennismuguongo@gmail.com>
2022-05-24 12:43:21 +0000

    new change on task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t dk;
+	int dk;
 	dk = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			dk++;
+		}
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		dk++;
 	}
