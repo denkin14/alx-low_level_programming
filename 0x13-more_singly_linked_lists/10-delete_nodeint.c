e944365ebc7e5de1ceef3381edbd2c466c4a36d0
denkin14 <dennismuguongo@gmail.com>
2022-05-27 14:33:53 +0000

    function deletes node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,3 +7,33 @@
   * Return: 1 if successful, -1 if it fails.
   */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count;
+	listint_t *next_node, *temp;
+
+	temp = *head;
+	count = 0;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+
+	while (temp != NULL && count < index - 1)
+	{
+		temp = temp->next;
+		count++;
+		if (temp == NULL || temp->next == NULL)
+			return (-1);
+	}
+
+	next_node = temp->next->next;
+	free(temp->next);
+	temp->next = next_node;
+
+	return (1);
+}
